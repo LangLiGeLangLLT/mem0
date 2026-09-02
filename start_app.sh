74db3ad23bec8fa82a674553d30ee89c0fb87094
llt <418797644@qq.com>
2026-09-03 00:44:23 +0800

    feat: update

---
+++ start_app.sh
@@ -0,0 +1,2 @@
+cd server
+uvicorn main:app --host 127.0.0.1 --port 8000 --reload
---
+++ start_install.sh
@@ -0,0 +1,5 @@
+cd server
+pip install -r requirements.txt
+pip install psycopg2
+pip install mem0ai[nlp]
+pip install en_core_web_sm
